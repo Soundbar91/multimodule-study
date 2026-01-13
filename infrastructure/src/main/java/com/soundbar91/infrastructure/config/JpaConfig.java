cc1deb9d535ad4c0645d9fae9a53de909fb24c57
soundbar91 <soundbar91@jinhakapply.com>
2026-01-13 17:38:44 +0900

    feat: 엔티티 스캔 범위 설정 추가

--- infrastructure/src/main/java/com/soundbar91/infrastructure/config/JpaConfig.java
+++ infrastructure/src/main/java/com/soundbar91/infrastructure/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.soundbar91.infrastructure.config;
 
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * JPA 설정
  */
 @Configuration
+@EntityScan(basePackages = "com.soundbar91.domain")
 @EnableJpaRepositories(basePackages = "com.soundbar91.infrastructure")
 public class JpaConfig {
 }
